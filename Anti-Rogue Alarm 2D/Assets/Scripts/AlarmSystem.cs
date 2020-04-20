da7b36cde79e85b7656c810d0ef1eb2cf80228a5
vaad23 <vaad23@mail.ru>
2020-04-20 19:43:27 +0300

    Create alpha version homework

---
+++ Anti-Rogue Alarm 2D/Assets/Scripts/AlarmSystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AlarmSystem : MonoBehaviour
+{
+    [SerializeField] private RoomAlarm _roomAlarm;
+
+    private Coroutine _changeVolume;
+    private AudioSource _sound;
+    private float _volumeChangeValue;
+    private bool _isActivated;
+
+    private void Awake()
+    {
+        _sound = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        _roomAlarm.AlarmActivated += Activation;
+        _roomAlarm.AlarmDeactivated += Deactivation;
+    }
+
+    private void OnDisable()
+    {
+        _roomAlarm.AlarmActivated -= Activation;
+        _roomAlarm.AlarmDeactivated -= Deactivation;
+    }
+
+    private void Activation()
+    {
+        if (_isActivated)
+            return;
+
+        _isActivated = true;
+        _sound.Play();
+        _sound.volume = 0.2f;
+        _volumeChangeValue = 0.01f;
+        _changeVolume = StartCoroutine(ChangeVolume());
+    }
+
+    private void Deactivation()
+    {
+        _isActivated = false;
+        _sound.Stop();
+        StopCoroutine(_changeVolume);
+    }
+
+    private IEnumerator ChangeVolume()
+    {
+        while (true)
+        {
+            if (_sound.volume == 1 || _sound.volume == 0)
+                _volumeChangeValue *= -1;
+
+            _sound.volume += _volumeChangeValue;
+
+            yield return null;
+        }
+    }
+}
---
+++ Anti-Rogue Alarm 2D/Assets/Scripts/RoomAlarm.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RoomAlarm : MonoBehaviour
+{
+    private int _countRogue;
+
+    public int CountRogue
+    {
+        get
+        {
+            return _countRogue;
+        }
+        private set
+        {
+            _countRogue = Mathf.Clamp(value, 0, int.MaxValue);
+
+            if (_countRogue == 0)
+                AlarmDeactivated?.Invoke();
+            else
+                AlarmActivated?.Invoke();
+        }
+    }
+
+    public event UnityAction AlarmActivated;
+    public event UnityAction AlarmDeactivated;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Rogue>(out Rogue rogue))
+        {
+            CountRogue++;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Rogue>(out Rogue rogue))
+        {
+            CountRogue--;
+        }
+    }
+}
